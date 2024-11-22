1a126837f731c0eac0806ab652c2e3fd6c773877
Yan Wallas <yanwallas98@gmail.com>
2024-11-22 08:48:14 -0300

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Logo from '../../assets/logo.png';
 import Button from '../button/Button';
-import Patrocinio from '../../pages/patrocinadores/Patrocinadores';
 
 function Header() {
   return (
